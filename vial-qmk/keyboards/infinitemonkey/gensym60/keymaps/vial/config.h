5a373c440f2208ad8f4edaed1f0d3a28d74bcb83
S. Ota <1632335+susumuota@users.noreply.github.com>
2026-06-17 03:09:00 +0900

    feat: add gensym60 vial qmk files

---
+++ vial-qmk/keyboards/infinitemonkey/gensym60/keymaps/vial/config.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xE5, 0x04, 0x11, 0xB1, 0x78, 0x77, 0x54, 0x59}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 1}
+#define VIAL_UNLOCK_COMBO_COLS {0, 0}
---
+++ vial-qmk/keyboards/infinitemonkey/gensym60/keymaps/vial/keymap.c
@@ -0,0 +1,18 @@
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
+        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UP,
+        MO(1),   KC_LALT, LGUI_T(KC_LNG2),  KC_SPC,  LT(1, KC_LNG1),   KC_LEFT, KC_DOWN, KC_RGHT
+    ),
+    [1] = LAYOUT(
+        KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_INS,  KC_DEL,
+        KC_TRNS, KC_NO,   KC_HOME, KC_UP,   KC_PGUP, KC_NO,   KC_NO,   KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_0, KC_NO,   KC_NO,   KC_TRNS,
+        KC_TRNS, KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,   KC_NO,   KC_KP_4, KC_KP_5, KC_KP_6, KC_NO,   KC_NO,   KC_TRNS,
+        KC_TRNS, KC_NO,   KC_END,  KC_NO,   KC_PGDN, KC_NO,   KC_KP_0, KC_KP_1, KC_KP_2, KC_KP_3, KC_NO,   KC_PGUP,
+        KC_CAPS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS,          KC_HOME, KC_PGDN, KC_END
+    ),
+};
